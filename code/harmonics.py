8cec9e61a88d204fe8571a144e744c09b972ec7e
Ryan Hamilton <ryangilberthamilton@gmail.com>
2023-02-08 13:54:49 -0500

    added harmonics wrapper

--- code/harmonics.py
+++ code/harmonics.py
@@ -1,5 +1,5 @@
 import math
-from typing import List
+from typing import Callable, List
 
 import ee
 
@@ -7,12 +7,23 @@ from . import cfg
 from . import inputs
 
 
+def add_harmonics(freq: List[int], cos_names: List[str], sin_names: List[str]) -> Callable:    
+    def add_harmonics_wrapper(element: ee.Image):
+        frequencies = ee.Image.constant(freq)
+        time = ee.Image(element).select('t')
+        cosines = time.multiply(frequencies).cos().rename(cos_names)
+        sines = time.multiply(frequencies).sin().rename(sin_names)
+        return element.addBands(cosines).addBands(sines)
+    return add_harmonics_wrapper
+
+
+
 class HarmonicRegression:
 
    
-    def __init__(self, harmonics_image: inputs.HarmonicsCollection, config: cfg.HarmonicCFG) -> None:
+    def __init__(self, config: cfg.HarmonicCFG) -> None:
         self.config = config
-        self.harmonics_image = harmonics_image
+        self.harmonics_image = config.collection
         self.harmonic_trend = self.harmonics_image.select([*self.config.independents, 
                                                            self.config.dependend]).\
             reduce(ee.Reducer.linearRegression(**{
@@ -29,9 +40,14 @@ class HarmonicRegression:
         def mk_phase_inner(numerator: str, denominator: str) -> ee.Image:
             return self._harmonic_coeff.select(numerator).atan(self._harmonic_coeff.\
                 select(denominator)).unitScale(-math.pi, math.pi)
-        return ee.Image.cat(*[mk_phase_inner(x,y) for x,y in zip(self.config.sin_names, 
+        
+        stack = ee.Image.cat(*[mk_phase_inner(x,y) for x,y in zip(self.config.sin_names, 
                                                                  self.config.cos_names)])
     
+        band_names: ee.List = stack.bandNames()
+        new_names = [f'phase_{idx}' for idx, _ in enumerate(self.config.sin_names)]
+        return stack.select(band_names, new_names)
+
     @property
     def ampltiude(self) -> ee.Image:
         
@@ -39,14 +55,18 @@ class HarmonicRegression:
             return self._harmonic_coeff.select(numerator)\
                 .hypot(self._harmonic_coeff.select(denominator))
         
-        return ee.Image.cat(*[mk_amp_inner(x,y) for x,y in zip(self.config.sin_names, 
+        stack = ee.Image.cat(*[mk_amp_inner(x,y) for x,y in zip(self.config.sin_names, 
                                                                 self.config.cos_names)])
-    
+        
+        band_names: ee.List = stack.bandNames()
+        new_names = [f'amp_{idx}' for idx, _ in enumerate(self.config.sin_names)]
+        return stack.select(band_names, new_names)
+
     @property
     def mean_dependent(self) -> ee.Image:
         return self.harmonics_image.select(self.config.dependend).mean()
     
-    def fit_harmonics(self) -> None:
+    def fit_harmonics(self) -> ee.ImageCollection:
         def fit_inner(element: ee.Image):
             return element.addBands(element.select(self.config.dependent).\
                 multiply(self.harmonic_coeff).\
@@ -54,6 +74,6 @@ class HarmonicRegression:
                 rename('fitted')
             )
     
-        return self._harmonics_image.map(fit_inner)
+        return self.harmonics_image.map(fit_inner)
         
     
